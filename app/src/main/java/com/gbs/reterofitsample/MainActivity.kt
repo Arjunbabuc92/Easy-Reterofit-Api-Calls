0dba4c19452e824ec7a8ac05475565af082b6934
Arjun Babu <arjunbc@gbs-plus.com>
2022-08-15 10:28:05 +0530

    module added

--- app/src/main/java/com/gbs/reterofitsample/MainActivity.kt
+++ app/src/main/java/com/gbs/reterofitsample/MainActivity.kt
@@ -5,9 +5,7 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.gbs.reterofitsample.model.login.LoginRequest
 import com.gbs.reterofitsample.model.login.LoginResponse
-import com.gbs.reterofit_network_calls.ApiCalls
 import com.gbs.reterofitsample.network.EndPoints
-import com.gbs.reterofit_network_calls.utils.ERROR
 import com.google.gson.Gson
 
 class MainActivity : AppCompatActivity() {
@@ -15,13 +13,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val apiCalls = ApiCalls()
-        val loginRequest = LoginRequest("1234567891@getnada.com","Ancy@1998")
-        apiCalls.getJson(EndPoints.baseURL+EndPoints.loginEndPoint,gson.toJson(loginRequest),this){
-            if(it.error==ERROR.NONE){
-                val loginResponse = gson.fromJson(it.responseData, LoginResponse::class.java)
-                Log.e("sample",loginResponse.toString())
-            }
-        }
+//        val apiCalls = ApiCalls()
+//        val loginRequest = LoginRequest("1234567891@getnada.com","Ancy@1998")
+//        apiCalls.getJson(EndPoints.baseURL+EndPoints.loginEndPoint,gson.toJson(loginRequest),this){
+//            if(it.error==ERROR.NONE){
+//                val loginResponse = gson.fromJson(it.responseData, LoginResponse::class.java)
+//                Log.e("sample",loginResponse.toString())
+//            }
+//        }
     }
 }
